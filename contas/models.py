6ce3b0ef0a1b65d958059c73ff9fa44e38b95973
Diego Martins Duarte <diegomartins836@gmail.com>
2021-01-18 22:59:09 -0300

    Atualizado models

--- contas/models.py
+++ contas/models.py
@@ -7,10 +7,20 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=200)
     data = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.nome
+
 class Transacao(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.CharField(max_length=200)
     data = models.DateTimeField()
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Transacoes'
+
+    def __str__(self) -> str:
+        return self.nome
+
 
