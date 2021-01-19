4c3c742787e306533b756c24ce39cd3c9e23aaad
Diego Martins Duarte <diegomartins836@gmail.com>
2021-01-18 22:49:02 -0300

    Projeto Criado

---
+++ contas/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=200)
+    data = models.DateTimeField(auto_now_add=True)
+
+class Transacao(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=200)
+    data = models.DateTimeField()
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=CASCADE)
+
