78a90071b0c6a146f206f3b1745a95b0cb360885
Diego Martins Duarte <diegomartins836@gmail.com>
2021-01-19 22:09:26 -0300

    Implementado metodo de listagem

--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Transacao
 
 # Create your views here.
 def home(request):
     return render(request, 'contas/home.html')
+
+def listar(request):
+    transacoes = Transacao.objects.all()
+    return render(request, 'contas/listar.html', {'transacoes':transacoes})
